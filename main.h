2a818c6a271aaea06d8f338e0afc712ab5be7d2a
CobraLover <6716@holbertonstudents.com>
2023-12-01 12:36:14 -0800

    main.h

--- main.h
+++ main.h
@@ -2,37 +2,35 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <limits.h>
 #include <unistd.h>
 
 /**
- * struct fmt_t - Struct op
- *
- * @fmt: The format specifier
- * @fct: The function associated.
+ * struct specifier - Struct to store information about format specifiers
+ * @c: Format specifier character
+ * @handler: Pointer to the function that handles the specifier
  */
 
-typedef struct fmt_t
+typedef struct specifier
 {
-	const char *fmt;
-	int (*fct)(va_list);
-} fmt_t;
+	char c;
+	int (*handler)(va_list args);
+} specifier_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int handle_specifier(char specifier, va_list args);
+int handle_format_string(const char *format, va_list args);
 
 /****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
-int print_address(void *ptr);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
+int _printf_address(void *ptr);
+int _printf_char(va_list args);
+int _printf_string(va_list args);
+int _printf_percent(va_list args);
 
 /* Functions to print numbers */
-int print_integer(va_list args);
-int print_unsigned_int(unsigned int num);
+int _printf_integer(va_list args);
+int _printf_unsigned_int(unsigned int num);
 
 #endif /* MAIN_H */
